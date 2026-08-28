01ca6ced0fb56d016f616ccd20688031b4527011
Rahul Iyer <rahulair@gmail.com>
2026-08-27 21:22:58 -0700

    Add backward guard

--- src/baby_pytorch/tensor.py
+++ src/baby_pytorch/tensor.py
@@ -271,6 +271,9 @@ class Tensor:
         return self
 
     def backward(self):
+        if self.data.ndim != 0:
+            raise ValueError("backward() only supported on scalar types")
+
         nodes = topo_sort(self)
         nodes.reverse()
 
